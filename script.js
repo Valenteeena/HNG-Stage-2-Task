2beb7b8be8a0c2d13d562cc140c605acf0534b3c
Adetoun Olayemi <valenteeena@gmail.com>
2021-08-18 08:54:54 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+//Variable with my name
+const myName = "Adetoun Olayemi Olawumi";
+
+//Element to Display the name in HTML
+let nameElement = document.getElementById("myName");
+
+//Display Name on the Webppage
+nameElement.innerHTML = myName;
+
+//Display name in console
+console.log(myName)
